8f773fc4e8dad89bd4c4bae6b56ad8c1255d08c3
leahmhb <blake.67@wright.edu>
2016-10-09 17:20:41 -0400

    functional test now checks that we can input a to-do item; updated readme

--- README.md
+++ README.md
@@ -4,20 +4,27 @@
 * [Test-Driven Development with Python][1] by Harry J. W. Percival
 * Python 3.5.2
 * Firefox 49.0
-* Selenium 3.0.03b
-* Django 1.10.0
+* [Selenium 3.0.03b][2]
+* [Django 1.10.0][3]
 
 ## Common Commands
-* python3 manage.py test
+* `python3 manage.py test`
     * Runs unit tests
-* python3 manage.py runserver
+* `python3 manage.py runserver`
     * Runs dev server
-* python3 functional_tests.py
+* `python3 functional_tests.py`
     * Runs functional tests
 
 ### Problems & Solutions
-* Potential differences from book since I am using Django 1.10 and the book uses Django 1.8
-* Ran into issues using Selenium2 with Firefox 49.0
-    * Solution: Upgrade to Selenium3 and use geckdriver
+* Ran into issues using Firefox 49.0
+    * Solution: geckodriver
+
+###Notes
+* I am using Django 1.10 and the book uses Django 1.8
+* I am using Selenium3 and the book uses Selenium2
+    * `from selenium.webdriver.common import Keys` becomes from `selenium.webdriver.common.keys import Keys`
+
 
 [1]: http://chimera.labs.oreilly.com/books/1234000000754/index.html
+[2]: https://pypi.python.org/pypi/selenium
+[3]: https://docs.djangoproject.com/en/1.10/
--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,14 +17,30 @@ class NewVisitorTest(unittest.TestCase):
 
     # Edit notices page title and header mention to-do lists
     self.assertIn('To-Do', self.browser.title)
-    self.fail('Finish the test!')
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('To-Do', header_text)
 
     # Edith is invited to enter to-do item right away
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(
+      inputbox.get_attribute('placeholder'),
+      'Enter a to-do item'
+      )
     # Edith types "Buy peacock feathers" into a textbox
+    inputbox.send_keys('Buy peacock feathers')
+
     # After hitting enter, page updates, page lists
     # '1: Buy peacock feathers' as an item in to-do list
+    inputbox.send_keys(Keys.enter)
+
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertTrue(
+      any(row.test == '1: Buy peacock feathers' for row in rows)
+      )
     # A textbox invites her to add another item
     # Edith types "Use peacock feathers to make a fly"
+    self.fail('Finish the test!')
     # Page updates again
     # Now shows both items on her list
     # Edith wonders whether the site will remember her list
